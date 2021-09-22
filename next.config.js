774f770eba9dae32909d0eddb400baf67b9794b5
jean sidharta <jeansidharta@gmail.com>
2021-09-22 01:31:52 -0300

    Bumped everything's version

--- next.config.js
+++ next.config.js
@@ -1,36 +1,23 @@
-const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const ImageminWebpWebpackPlugin = require(`imagemin-webp-webpack-plugin`);
 
 module.exports = {
-	future: {
-		webpack5: true,
-	},
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
+	async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
 		return {
-		...defaultPathMap,
-		// Add your reroutes here. Example:
-		// Reroutes the '/' path to '/home'
-		// '/': { page: '/home' },
-		}
+			...defaultPathMap,
+			// Add your reroutes here. Example:
+			// Reroutes the '/' path to '/home'
+			// '/': { page: '/home' },
+		};
 	},
 
 	// Custom webpack configurations
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.module.rules.push({
-			// Change this regex if you want another type of file to be interpred by webpack
-			test: /\.(png|jpe?g|gif|svg)$/i,
+			test: /\.(png|jpe?g)$/i,
 			use: [
 				{
-					// Allows for any kind of file to be imported through webpack
-					loader: 'file-loader',
-					options: {
-						publicPath: `/_next/static/images/`,
-						outputPath: `${isServer ? "../" : ""}static/images/`,
-						name: "[name]-[hash].[ext]",
-						esModule: false,
-					}
+					// Allows for png and jpeg resizing
+					loader: `webpack-image-resize-loader`,
 				},
 			],
 		});
@@ -39,4 +26,4 @@ module.exports = {
 		config.plugins.push(new ImageminWebpWebpackPlugin());
 		return config;
 	},
-}
+};
